c7525528b1ae3e23d45eddd9a8e3d0a528423818
Lawrence <lawrence.langen@gmail.com>
2023-02-06 10:45:52 +0100

    First Commit

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -4,7 +4,21 @@ public class App {
     
     public static void main(String[] args) {
      
+    Queen queen = new Queen();
+    Worker worker = new Worker();
+    Drone drone = new Drone();
+
+        pollObj(queen);
+        pollObj(worker);
+        pollObj(drone);
+ 
     }
+
+    private static void pollObj(HoneyBee bee) {
+		output(bee.doYourJob());
+        output(bee.fly());
+		
+	} 
 
     private static void output(String outputStr) {
         System.out.println(outputStr);
---
+++ com/cc/java/Drone.java
@@ -0,0 +1,9 @@
+package com.cc.java;
+
+public class Drone extends HoneyBee {
+    
+    public String doYourJob() {
+        return "I'm a Drone, I'm going to date our Queen!";
+
+    }
+}
